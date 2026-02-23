57f06b8c8a04b1673bd23ea37d3b24f4d6cf69e6
boss566y <67247583+boss566y@users.noreply.github.com>
2026-02-23 11:36:40 -0400

    Update docker-update.sh
    add checks to skip folders without docker-compose.yml.
    
    add option to set update choice in .docker_update_choice file.

--- docker-update.sh
+++ docker-update.sh
@@ -6,17 +6,40 @@ cd $stacks_locations
 
 docker_folders=$(ls -d */)
 
-for folder in $docker_folders
-	do
-		cd $folder
-		echo "Update? $folder"
-		select option in "Yes" "No"; do
-		choice=${option:-$REPLY}
-			case $choice in
-				Yes | yes | Y | y ) docker compose pull && docker compose down && docker compose up -d ; break;;
-				No | no | N | n ) break;;
-				* ) break;;
-			esac
-		done
-		cd ..
+docker_update() {
+	docker compose pull && docker compose down && docker compose up -d
+}
+
+
+for folder in $docker_folders; do
+	# RESETS
+	cd ~/docker/$folder
+
+	docker_update_choice=""
+
+	# CHECKS
+	if [ ! -f ./docker-compose.yml ]; then
+		continue
+	fi
+
+	if [ -f ./.docker_update_choice ]; then
+		docker_update_choice=$(cat ./.docker_update_choice)
+	fi
+
+	# DOCKER UPDATES
+	case $docker_update_choice in
+		Yes | yes ) docker_update ; continue;;
+		No | no ) continue;;
+		* ) ;;
+	esac
+
+	echo "Update? $folder"
+	select option in "Yes" "No"; do
+	choice=${option:-$REPLY}
+		case $choice in
+			Yes | yes | Y | y ) docker_update ; break;;
+			No | no | N | n ) break;;
+			* ) break;;
+		esac
 	done
+done
